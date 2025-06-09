096db6d4fb102800c7545cf9e26ef997ff0ca143
eunchrri <leesilver7@gmail.com>
2025-06-09 16:57:35 +0900

    Feat: [EVER-104] 출석 기능 api 구현 (출석 등록, 연속 출석 조회, 출석률 조회)

---
+++ src/main/java/com/team4ever/backend/domain/attendance/dto/AttendanceRequest.java
@@ -0,0 +1,8 @@
+package com.team4ever.backend.domain.attendance.dto;
+
+import lombok.Getter;
+
+@Getter
+public class AttendanceRequest {
+    private Long userId;
+}
---
+++ src/main/java/com/team4ever/backend/domain/attendance/service/AttendanceService.java
@@ -0,0 +1,55 @@
+package com.team4ever.backend.domain.attendance.service;
+
+import com.team4ever.backend.domain.attendance.entity.Attendance;
+import com.team4ever.backend.domain.attendance.dto.AttendanceDto;
+import com.team4ever.backend.domain.attendance.repository.AttendanceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AttendanceService {
+    private final AttendanceRepository attendanceRepository;
+
+    public AttendanceDto checkToday(Long userId) {
+        LocalDate today = LocalDate.now();
+        boolean exists = attendanceRepository.existsByUserIdAndCheckedDate(userId, today);
+        if(exists) {
+            throw new RuntimeException("이미 출석했습니다.");
+        }
+
+        Attendance saved = attendanceRepository.save(new Attendance(userId, today));
+        return AttendanceDto.from(saved);
+    }
+
+    public int getStreak(Long userId) {
+        List<Attendance> list = attendanceRepository.findAllByUserIdOrderByCheckedDateDesc(userId);
+        int streak = 0;
+        LocalDate today = LocalDate.now();
+
+        for (Attendance a : list) {
+            if (a.getCheckedDate().equals(today.minusDays(streak))) {
+                streak++;
+            } else {
+                break;
+            }
+        }
+        return streak;
+    }
+
+    public double calculateRate(Long userId) {
+        LocalDate today = LocalDate.now();
+        LocalDate startOfMonth = today.withDayOfMonth(1); // 이번 달 1일
+        List<Attendance> records = attendanceRepository.findByUserIdAndCheckedDateBetween(userId, startOfMonth, today);
+
+        int attendedDays = records.size();
+        long daysPassed = ChronoUnit.DAYS.between(startOfMonth, today) + 1; // 1일 더해야 하므로 +1
+
+        return Math.round(attendedDays * 100.0 / daysPassed) * 10 / 10.0;
+    }
+
+}
