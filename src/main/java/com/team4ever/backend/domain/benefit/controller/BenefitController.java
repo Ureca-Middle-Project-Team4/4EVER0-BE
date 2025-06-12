902021de37598e1542d723d7cf8a561beeed1976
jihoi0615 <pjhalice0615@gmail.com>
2025-06-12 13:38:17 +0900

    Feat: [EVER-109] dto 추가 #58

---
+++ src/main/java/com/team4ever/backend/domain/benefit/controller/BenefitController.java
@@ -0,0 +1,31 @@
+package com.team4ever.backend.domain.benefit.controller;
+
+import com.team4ever.backend.domain.benefit.dto.BenefitResponse;
+import com.team4ever.backend.domain.benefit.service.BenefitService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDate;
+import java.util.List;
+import com.team4ever.backend.domain.benefit.dto.BenefitApiResponse;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/uplus-benefits")
+public class BenefitController {
+
+    private final BenefitService benefitService;
+
+    @GetMapping
+    public BenefitApiResponse<List<BenefitResponse>> getAllBenefits() {
+        return BenefitApiResponse.ok("요청이 성공적으로 처리되었습니다.", benefitService.getAllBenefits());
+    }
+
+    @GetMapping(params = "date")
+    public BenefitApiResponse<List<BenefitResponse>> getBenefitsByDate(@RequestParam String date) {
+        LocalDate parsedDate = LocalDate.parse(date);
+        return BenefitApiResponse.ok("요청이 성공적으로 처리되었습니다.", benefitService.getBenefitsByDate(parsedDate));
+    }
+}
