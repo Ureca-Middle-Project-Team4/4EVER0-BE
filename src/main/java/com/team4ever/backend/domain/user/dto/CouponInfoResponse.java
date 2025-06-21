57af9e9baf7e903d408bb7d68f1af20510bbcd16
illustermin <2002hongmj@naver.com>
2025-06-22 02:42:42 +0900

    Feat: [EVER-233] [EVER-233] 쿠폰에 startDate, endDate 추가 구현

--- src/main/java/com/team4ever/backend/domain/user/dto/CouponInfoResponse.java
+++ src/main/java/com/team4ever/backend/domain/user/dto/CouponInfoResponse.java
@@ -4,7 +4,9 @@ public record CouponInfoResponse(
     Long couponId,
     String title,
     BrandInfo brand,
-    boolean isUsed
+    boolean isUsed,
+    String startDate,
+    String endDate
 ) {
     public record BrandInfo(Long id, String name, String imageUrl) {}
 }
